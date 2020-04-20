332b2664aad1a7f845f620cad176108a372e6cbe
SirVsevolod <19vsevolod97@gmail.com>
2020-04-20 20:59:04 +0300

    parsing

--- parsing.py
+++ parsing.py
@@ -4,6 +4,11 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = 'https://www.avito.ru/fryazino?q=%D0%BD%D0%BE%D1%83%D1%82%D0%B1%D1%83%D0%BA%D0%B8'
+
+
+		
+
+
 HEADERS = {'user-agent':'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0'
 ,'accept':'*/*'}
 HOST = 'https://www.avito.ru'
@@ -13,12 +18,21 @@ def get_html(url,params = None):
 	r = requests.get(url, headers = HEADERS, params = params)
 	return r
 
+def get_pages_count(html):
+	soup = BeautifulSoup(html,'html.parser')
+	pagination = soup.find_all('span',class_ = 'pagination-item-1WyVp')
+	if pagination:
+		return int(pagination[-2].get_text())
+	else:
+		return 1
+	print(pagination)
+
 
 
 def get_content(html):
 	soup = BeautifulSoup(html,'html.parser')
 	items = soup.find_all('div',class_='snippet-horizontal')
-	#print(items)
+	
 
 	cars = []
 
@@ -42,17 +56,25 @@ def get_content(html):
 			'title': item.find('a',class_='snippet-link').get_text(strip = True),
 			'link': HOST + item.find('a',class_='snippet-link').get('href'),
 			'price':price,
-			'street': street
+			'street': street 
 			})
-	print(cars)
+	return cars
+	#print("get_content")
 
 
 
 def parse():
 	html = get_html(URL)
 	if html.status_code == 200:
-		get_content(html.text)
+		cars = []
+		pages_count = get_pages_count(html.text)
+
+		for page in range(1,pages_count+1):
+			print(f'загрузка страницы {page} из {pages_count}...')
+			html = get_html(URL, params = {'p': page})
+			cars.extend(get_content(html.text))
+		print(cars)
 	else:
 		print("Error")
 
-parse()
+parse()	
